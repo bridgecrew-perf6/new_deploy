a92201635cdfe8ee7eb4e40564169a3b9b0c583f
student <vineelvin0604@gmail.com>
2022-06-05 17:24:43 +0000

    new

---
+++ src/main/java/com/example/new_deploy/NewDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_deploy/NewDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
